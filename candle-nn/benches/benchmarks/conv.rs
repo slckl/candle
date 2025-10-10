0636b69c7483f2d8a0cf60df4ebb3e5ef9dc0235
Andris Vāravs <andris.varavs@lmt.lv>
2025-10-11 00:15:05 +0300

    candle-nn/conv benches: more cases, variable kernel sizes, bias/no bias

--- candle-nn/benches/benchmarks/conv.rs
+++ candle-nn/benches/benchmarks/conv.rs
@@ -8,20 +8,28 @@ const B: usize = 1;
 const C: usize = 1;
 const M: usize = 128;
 const K: usize = 128;
-const K_SIZE: usize = 3;
 
-fn run(input: Tensor, weight: Tensor, bias: Tensor, config: Conv2dConfig) {
-    Conv2d::new(weight, Some(bias), config)
-        .forward(&input)
-        .unwrap();
+fn run(input: Tensor, weight: Tensor, bias: Option<Tensor>, config: Conv2dConfig) {
+    Conv2d::new(weight, bias, config).forward(&input).unwrap();
 }
 
-fn run_conv2d_benchmark(c: &mut Criterion, device: &Device, dtype: DType, name: &str) {
-    let weight = Tensor::ones((1, 1, K_SIZE, K_SIZE), dtype, device)
+fn run_conv2d_benchmark(
+    c: &mut Criterion,
+    device: &Device,
+    dtype: DType,
+    k_size: usize,
+    bias: bool,
+    name: &str,
+) {
+    let weight = Tensor::ones((1, 1, k_size, k_size), dtype, device)
         .unwrap()
         .to_dtype(dtype)
         .unwrap();
-    let bias = Tensor::zeros(K, dtype, device).unwrap();
+    let bias = if bias {
+        Some(Tensor::zeros(M, dtype, device).unwrap())
+    } else {
+        None
+    };
     let input = Tensor::ones((B, C, M, K), dtype, device).unwrap();
 
     let mut group = c.benchmark_group(device.bench_name(name));
@@ -46,8 +54,15 @@ fn run_conv2d_benchmark(c: &mut Criterion, device: &Device, dtype: DType, name: 
 fn criterion_benchmark(c: &mut Criterion) {
     let device = BenchDeviceHandler::new().unwrap();
     for d in device.devices {
-        run_conv2d_benchmark(c, &d, DType::F32, "conv2d_f32");
-        run_conv2d_benchmark(c, &d, DType::F16, "conv2d_f16");
+        // run_conv2d_benchmark(c, &d, DType::F32, 1, true, "conv2d_f32_1x1");
+        // run_conv2d_benchmark(c, &d, DType::F32, 3, true, "conv2d_f32_3x3");
+        // run_conv2d_benchmark(c, &d, DType::F32, 5, true, "conv2d_f32_5x5");
+        run_conv2d_benchmark(c, &d, DType::F32, 3, false, "conv2d_f32_3x3_no_bias");
+        run_conv2d_benchmark(c, &d, DType::F32, 5, false, "conv2d_f32_5x5_no_bias");
+        // run_conv2d_benchmark(c, &d, DType::F16, 1, true, "conv2d_f16_1x1");
+        // run_conv2d_benchmark(c, &d, DType::F16, 3, true, "conv2d_f16_3x3");
+        // run_conv2d_benchmark(c, &d, DType::F16, 5, true, "conv2d_f16_5x5");
+        run_conv2d_benchmark(c, &d, DType::F16, 5, false, "conv2d_f16_5x5_no_bias");
     }
 }
 
